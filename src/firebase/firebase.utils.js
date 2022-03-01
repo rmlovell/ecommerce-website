d28dabda026cd0090bd7648c146125b5ae4d8ea4
Robin <robin.lovell@perficient.com>
2022-03-01 15:06:47 -0500

    implemented firebase utils, included ability to store authenticated users into our firestore database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,13 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: "AIzaSyBlTQYzsjhkkDZOaZFHyBclY7cAOCimnt4",
+  authDomain: "crwn-db-4213c.firebaseapp.com",
+  projectId: "crwn-db-4213c",
+  storageBucket: "crwn-db-4213c.appspot.com",
+  messagingSenderId: "795668359612",
+  appId: "1:795668359612:web:ce1d58ebc250485bbbe3b9",
+  measurementId: "G-FDBX16RK2G"
 };
 
 firebase.initializeApp(config);
